bd826c000555caea61338816078c49918bdbfbd2
Danny Archer <danny@inbeta.org>
2013-04-11 08:44:58 +1000

    changing names and adding new return type, not yet implemented

--- pymdb.py
+++ pymdb.py
@@ -1,9 +1,10 @@
 import pymdbbin
 
-class Strategy:
-  table_dictionary = range(1)
+class PymdbStrategy:
+  table_dictionary  = 1
+  objects           = 2
   
-def parsefile(file, parsestrategy=Strategy.table_dictionary):
-    if(parsestrategy==Strategy.table_dictionary):
+def parsefile(file, parsestrategy=PymdbStrategy.table_dictionary):
+    if(parsestrategy==PymdbStrategy.table_dictionary):
         return pymdbbin.parsefile_table_dictionary(file)
     return None
